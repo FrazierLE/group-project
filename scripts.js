817750c93294e2fd1e789f7cf3f3b7aac2a8155d
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-09-13 08:48:28 -0600

    Psuedocode color class

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// Color:
+// A color should have a random hex code
+// hint: hex codes are 6 characters long, and each character is some value of 0-9 or A-F (ABCDEF0123456789)
+// Though there are many examples of this logic coded out, this type of crunchy problem solving is well within your skill set!
+// Don’t look up how to accomplish this; challenge yourselves to use pseudocode to problem-solve through it!
+// It should have a property of locked, whose value is a boolean. Colors begin unlocked.
+
+class Color {
+  constructor() {
+
+  }
+}
+
+
+//pseudocode
+//goal: be able to generate a random hex code
+//data: onclick invoke function that will need to generate a string with 6 characters containing values between 0-9 and A-F
+  //will need to update to h3 box
+//questions: are there existing methods that generate random? Should we create an array with 0-9 and A-F? What should hex codes look like -- what's the format?
