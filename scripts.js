5b6f80f837f08e1a638dad3abac8cccc4ccebc23
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-09-14 16:06:59 -0700

    complete page load with random colors and hexcode labels

--- scripts.js
+++ scripts.js
@@ -20,10 +20,24 @@ var box1Color = document.querySelector("#box-1");
 var box1Label = document.querySelector("#color-1");
 var box2Color = document.querySelector("#box-2");
 var box2Label = document.querySelector("#color-2");
+var box3Color = document.querySelector("#box-3");
+var box3Label = document.querySelector("#color-3");
+var box4Color = document.querySelector("#box-4");
+var box4Label = document.querySelector("#color-4");
+var box5Color = document.querySelector("#box-5");
+var box5Label = document.querySelector("#color-5");
 
 
 box1Color.style.background = testPalette.colors[0].hexCode;
 box1Label.innerText = testPalette.colors[0].hexCode;
+box2Color.style.background = testPalette.colors[1].hexCode;
+box2Label.innerText = testPalette.colors[1].hexCode;
+box3Color.style.background = testPalette.colors[2].hexCode;
+box3Label.innerText = testPalette.colors[2].hexCode;
+box4Color.style.background = testPalette.colors[3].hexCode;
+box4Label.innerText = testPalette.colors[3].hexCode;
+box5Color.style.background = testPalette.colors[4].hexCode;
+box5Label.innerText = testPalette.colors[4].hexCode;
 
 
 //GOAL: when the user first visits the page,
